105a557886207bd681802018b9cccdfd21ad27a8
Shubham <shubham.tapmod@gmail.com>
2019-02-05 03:41:02 +0530

    11 Data read from BlackBoard in C++

--- Source/S5_Testing_Grounds/Private/ChoseNextWaypoint.cpp
+++ Source/S5_Testing_Grounds/Private/ChoseNextWaypoint.cpp
@@ -1,9 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChoseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChoseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in CPP"));
+	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
--- Source/S5_Testing_Grounds/Public/ChoseNextWaypoint.h
+++ Source/S5_Testing_Grounds/Public/ChoseNextWaypoint.h
@@ -14,4 +14,8 @@ class S5_TESTING_GROUNDS_API UChoseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };
